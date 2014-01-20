5a212d41bf15f9a1b87ff9b2a9935c4a63a4575e
Anton Nikulin <dr.tallin@gmail.com>
2014-01-20 23:45:40 +0200

    + Adding Article

--- Article/forms.py
+++ Article/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from .models import Article
 
 class ArticleAddForm(forms.Form):
-    title = forms.CharField(required=True)
-    body = forms.Textarea(required=True)
+    Title = forms.CharField(required=True)
+    Body = forms.CharField(widget=forms.Textarea)
     tags = forms.CharField()
--- Article/views.py
+++ Article/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import ArticleAddForm
+
+
+@login_required()
+def articleAdd(request):
+    if request.method == "POST":
+        form = ArticleAddForm(request.POST)
+        if form.is_valid():
+            #create new article
+            title = form.cleaned_data["Title"]
+            body = form.cleaned_data["Body"]
+            article = Article(
+                Title = title, Body = body
+            )
+            article.save()
+            tags = form.cleaned_data["tags"].split()
+            for tagName in tags:
+                tag, created = Tag.objects.get_or_create(Name = tagName)
+                article.tag_set.add(tag)
+            article.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render( request, "Article/articleAdd.html", {"form": form,} )
+    else:
+        form = ArticleAddForm()
+        return render( request, "Article/articleAdd.html", {"form": form,} )
