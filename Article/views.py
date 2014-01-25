5dd039e0ed50de0caa52f241f8539753928782ab
Anton Nikulin <dr.tallin@gmail.com>
2014-01-25 19:37:19 +0200

    + comments

--- Article/views.py
+++ Article/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
@@ -7,6 +9,11 @@ from .forms import ArticleAddForm
 
 @login_required()
 def articleAdd(request):
+    """
+    GET: пустая форма
+    POST: добаляет в БД новую запись "article". Связывая указаные имена тагов из поля "tags" формы с записями tag в БД
+    если таковые присутсвуют и добавляет в противном случае.
+    """
     if request.method == "POST":
         form = ArticleAddForm(request.POST)
         if form.is_valid():
@@ -18,6 +25,7 @@ def articleAdd(request):
             )
             article.save()
             tags = form.cleaned_data["tags"].split()
+            #get tags from DB and add to article
             for tagName in tags:
                 tag, created = Tag.objects.get_or_create(Name = tagName)
                 article.tag_set.add(tag)
@@ -31,5 +39,10 @@ def articleAdd(request):
 
 
 def articleRead(request, articleID):
+    """
+    Страница для отображения полного текста статьи
+
+    :param articleID: ID статьи
+    """
     article = get_object_or_404(Article, pk=articleID)
     return render( request, "Article/articleRead.html", {"article": article,} )
